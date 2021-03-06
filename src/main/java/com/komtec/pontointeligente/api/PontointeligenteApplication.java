de8d7c0b9d6bf5650c0fe50ce42d00e4d910e4c3
KomTec <komtec.komatsu@gmail.com>
2021-03-06 09:47:09 -0300

    Adicionado serviços

--- src/main/java/com/komtec/pontointeligente/api/PontointeligenteApplication.java
+++ src/main/java/com/komtec/pontointeligente/api/PontointeligenteApplication.java
@@ -2,8 +2,12 @@ package com.komtec.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles
 @SpringBootApplication
+@EnableCaching
 public class PontointeligenteApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/komtec/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.komtec.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
